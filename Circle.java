d45eb9ec02a09e4041dae3a5189a24b66dbe1b88
Sajal <sajalsingh2704@gmail.com>
2025-06-10 21:55:45 +0530

    Initial commit

---
+++ Circle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Circle {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the radius of the circle: ");
+double radius = sc.nextDouble();       
+double area = Math.PI * radius * radius; 
+
+System.out.println("The area of the circle is: " + area);
+
+sc.close();  
+}
+}
