f6c87b954edf63606757e4b5a4cb01d4308fabf1
Sajal <sajalsingh2704@gmail.com>
2025-06-11 17:20:36 +0530

    [Sajal] average marks

---
+++ Day1/Sam.java
@@ -0,0 +1,12 @@
+class Sam {
+public static void main(String[] args) {
+int maths = 94;
+int physics = 95;
+int chemistry = 96;
+
+int marks = maths + physics + chemistry;
+int average = marks / 3;
+
+System.out.println("Sam average mark in PCM is " + average);
+}
+}
