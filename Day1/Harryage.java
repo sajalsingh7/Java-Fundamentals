7af6b6cf74a341f1ad2c5dbd789abe7ab6a9e22a
Sajal <sajalsingh2704@gmail.com>
2025-06-11 15:50:22 +0530

    [Sajal] find age of harry

---
+++ Day1/Harryage.java
@@ -0,0 +1,9 @@
+class Harryage {
+public static void main(String[] args) {
+int birth = 2000;
+int current = 2024;
+int age = current - birth;
+
+System.out.println("Harry age in 2024 is " + age);
+}
+}
