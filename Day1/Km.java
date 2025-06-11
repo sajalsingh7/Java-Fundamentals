5dfc1bab014be5c733dca5f97d51e8eaab276cde
Sajal <sajalsingh2704@gmail.com>
2025-06-11 17:56:33 +0530

    [Sajal] convert km to miles

---
+++ Day1/Km.java
@@ -0,0 +1,10 @@
+class Km {
+public static void main(String[] args) {
+double km = 10.8;
+double convert = 1.6;
+
+double miles = km * convert;
+
+System.out.println("The distance " + km + " km in miles is " + miles);
+}
+}
